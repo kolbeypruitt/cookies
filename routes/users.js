f002d3975530b1b6c9d8d4210dfab44f836dd6b3
kolbeypruitt <kolbeypruitt@gmail.com>
2015-10-07 15:51:41 -0600

    it works

--- routes/users.js
+++ routes/users.js
@@ -2,6 +2,7 @@ var express = require('express');
 var router = express.Router();
 var db = require('monk')('localhost/cookiedb');
 var cookiedb = db.get('users');
+var checkdbEmail = 'cookiedb.find({email: req.body.email, password: req.body.password}).query'
 
 /* GET users listing. */
 router.get('/', function (req, res, next) {
@@ -30,26 +31,31 @@ router.get('/login', function (req, res, next) {
 
 // read cookie // req.cookies.email
 router.post('/login', function (req, res, next) {
-  // res.cookie('email', req.body.email);
-  // res.cookie('password', req.body.password);
-  cookiedb.find({email: req.body.email, password: req.body.password}, function (err, credentials) {
-    res.redirect('loggedin');
+  cookiedb.findOne({email: req.body.email, password: req.body.password}, function (err, user) {
+    if (user) {
+      res.cookie('email', req.body.email);
+      res.cookie('password', req.body.password);
+      res.redirect('loggedin');
+    }
+    else {
+      res.redirect('login');
+    }
   });
 });
 
 router.get('/loggedin', function (req, res, next) {
-  res.render('loggedin', {email: req.cookies.fraggle, title: 'You have been securely logged into our super-secure network'})
+  res.render('loggedin', {email: req.cookies.email, title: 'You have been securely logged into our super-secure network'});
 });
 
 module.exports = router;
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Cookies'});
-  res.cookie('fraggle', req.query.foo)
-  res.render('index', { title: req.query.foo });
-});
+// /* GET home page. */
+// router.get('/', function(req, res, next) {
+//   // res.render('index', { title: 'Cookies'});
+//   // res.cookie('fraggle', req.query.foo)
+//   res.render('index', { title: req.query.foo });
+// });
 
-router.get('/show', function(req, res, next) {
-  res.render('index', { title: req.cookies.fraggle });
-});
+// router.get('/show', function(req, res, next) {
+//   res.render('index', { title: req.cookies.fraggle });
+// });
